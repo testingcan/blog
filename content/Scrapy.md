0ac6e377b0bdf0895dcd749d4b51678a1b95d02c
= <raphael.wuillemier@wuillemier.com>
2017-11-22 10:27:38 +0100

    Inital commit with blog

---
+++ content/Scrapy.md
@@ -0,0 +1,8 @@
+Title: Crawling dynamically generated content with Scrapy
+Date: 2017-11-21
+Category: Tutorial
+Tags: scrapy, python
+Summary: Learn how to crawl dynamic pages with Scrapy.
+
+
+The following will help anyone who wants to crawl websites!
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'testingcan'
+SITENAME = 'Alldata'
+SITEURL = ''
+
+THEME = "/Users/rwu/Documents/scripts/pelican/themes/pelican-clean-blog"
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+LOAD_CONTENT_CACHE = True
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
